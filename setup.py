253641f65162becbc54ef7459e45564324679546
TaylorSMarks <taylor@marksfam.com>
2020-09-05 10:14:04 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+version = '0.1.0'
+
+setup(
+    name='classroom_sync',
+    version=version,
+    url='https://github.com/TaylorSMarks/classroom_sync/',
+    author='Taylor S. Marks',
+    author_email='tayor@marksfam.com',
+    description='A Thonny Plugin for easily sharing code in an online classroom.',
+    packages = ['thonnycontrib'],
+    platforms='any',
+    install_requires=[
+        'requests'
+    ]
+)
